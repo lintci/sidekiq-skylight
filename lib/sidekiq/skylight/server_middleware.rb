810de608734b4c8833ceafb885c79dfdcf6d6c15
Jeff Felchner <accounts+git@thekompanee.com>
2015-07-11 01:15:01 -0500

    Added: Blacklisting of certain workers
    This is important for some users who have jobs which run an exceedingly large
    number of times per day.  For example, if a worker runs every second or every
    5 seconds.
    
    Since skylight charges by the request, it's economical to filter these frequent
    workers from the list.
    
    --------------------------------------------------------------------------------
    Actions:
      * Closes #2

--- lib/sidekiq/skylight/server_middleware.rb
+++ lib/sidekiq/skylight/server_middleware.rb
@@ -4,9 +4,15 @@ module Sidekiq
   module Skylight
     class ServerMiddleware
       def call(worker, job, queue)
+        return if config.blacklisted_workers.include? worker.class.name
+
         ::Skylight.trace("#{worker.class.to_s}#perform", 'app.sidekiq.worker', 'process') do
           yield
         end
+      end
+
+      def config
+        Sidekiq::Skylight.config
       end
     end
   end
